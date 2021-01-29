8114190e30d5d6f1f5323f6ebd503e189f5ea43e
Marian <marian@luschnat.de>
2021-01-29 15:41:57 +0100

    Add small changes to authMiddleware

--- app/authMiddleware.go
+++ app/authMiddleware.go
@@ -18,14 +18,18 @@ func (am AuthMiddleware) authorizationHandler() func(http.Handler) http.Handler 
 			currentRouteName := mux.CurrentRoute(r).GetName()
 			if !((currentRouteName == "GetUser") || (currentRouteName == "UpdateUserWeights")) {
 				next.ServeHTTP(w, r)
+				return
 			} else if authHeader := r.Header.Get("Authorization"); authHeader == "" {
 				appError := errs.NewUnauthorizedError("Token missing")
 				writeResponse(w, appError.Code, appError.AsMessage())
+				return
 			} else if tokenUserId, appError := am.service.Verify(authHeader); appError != nil {
 				writeResponse(w, appError.Code, appError.AsMessage())
+				return
 			} else if *tokenUserId != mux.Vars(r)["id"] {
 				appError := errs.NewUnauthorizedError("Token not matching requested user")
 				writeResponse(w, appError.Code, appError.AsMessage())
+				return
 			} else {
 				next.ServeHTTP(w, r)
 			}
